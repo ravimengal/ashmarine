2c05de758fb0ec7a5fba3435ee684337d57efc9b
ravimengal <mengal22ravi@gmail.com>
2021-11-19 07:04:18 -0800

    added some pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
 import Home from './Components/Home/Home';
 
 
@@ -8,6 +10,8 @@ function App() {
     <div className="App">
      
       <Home/>
+      <About/>
+      <Contact/>
     </div>
   );
 }
