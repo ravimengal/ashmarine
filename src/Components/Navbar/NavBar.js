7f7b53e4053434ca4fd9c399ba3333222c9fcfd6
ravimengal <mengal22ravi@gmail.com>
2021-12-02 06:34:17 -0800

    Navigation setup

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,76 +1,26 @@
-import React from 'react'
-import { Navbar, Nav, Container} from 'react-bootstrap';
-import Welcome from '../WelcomePage/Welcome';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
 const NavBar = () => {
-    return (
-        <div>
-      <nav id='menu' className='navbar navbar-default navbar-fixed-top'>
-      <div className='container'>
-        <div className='navbar-header'>
-          <button
-            type='button'
-            className='navbar-toggle collapsed'
-            data-toggle='collapse'
-            data-target='#bs-example-navbar-collapse-1'
-          >
-            {' '}
-            <span className='sr-only'>Toggle navigation</span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-          </button>
-          <a className='navbar-brand page-scroll' href='#page-top'>
-            React Landing Page
-          </a>{' '}
-        </div>
+  return (
+    <div>
+      <Navbar fixed='top' collapseOnSelect expand="lg"  variant="light">
+      <Container fluid>
+          <Navbar.Brand to="/">ASMH</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            
+            <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/ourvision">OurVision</Nav.Link>
+              <Nav.Link href="/weoffers">Weoffers</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    
+    </div>
+  );
+};
 
-        <div
-          className='collapse navbar-collapse'
-          id='bs-example-navbar-collapse-1'
-        >
-          <ul className='nav navbar-nav navbar-right'>
-            <li>
-              <a href='#features' className='page-scroll'>
-                Features
-              </a>
-            </li>
-            <li>
-              <a href='#about' className='page-scroll'>
-                About
-              </a>
-            </li>
-            <li>
-              <a href='#services' className='page-scroll'>
-                Services
-              </a>
-            </li>
-            <li>
-              <a href='#portfolio' className='page-scroll'>
-                Gallery
-              </a>
-            </li>
-            <li>
-              <a href='#testimonials' className='page-scroll'>
-                Testimonials
-              </a>
-            </li>
-            <li>
-              <a href='#team' className='page-scroll'>
-                Team
-              </a>
-            </li>
-            <li>
-              <a href='#contact' className='page-scroll'>
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-      <Welcome/>
-      </div>
-    )
-}
-
-export default NavBar
+export default NavBar;
